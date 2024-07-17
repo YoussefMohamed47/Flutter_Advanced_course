75236f11bbffb8955ab9d7425d43c2cdc45c316c
Youssef Mohamed <youssef.mohammed@encooders.com>
2024-07-17 15:44:16 +0300

    Login Screen UI - made Custom widget For already_have_account_text.dart and terms_and_conditions_text.dart
    -- Custom Widgets
    - made an appTextButton
    - made an app TextFormField
    
    --Styles
    - adding more Colors
    - making custom widget for font style by each weight
    - adding more fontStyles

---
+++ lib/features/login/ui/widgets/terms_and_conditions_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/theming/styles.dart';
+
+class TermsAndConditionsText extends StatelessWidget {
+  const TermsAndConditionsText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'By signing up you agree to our ',
+            style: TextStyles.font13DarkBlueMedium,
+          ),
+          TextSpan(
+            text: ' Terms & Conditions',
+            style: TextStyles.font13DarkBlueMedium,
+          ),
+          TextSpan(
+            text: ' and ',
+            style: TextStyles.font13GreyRegular.copyWith(height: 1.5),
+          ),
+          TextSpan(
+            text: ' Privacy Policy',
+            style: TextStyles.font13DarkBlueMedium,
+          ),
+        ],
+      ),
+    );
+  }
+}
